4d7e8af8b454457c9aaa27d59a7e4b18997da2b9
ingo <ingo.walz@googlemail.com>
2013-03-08 23:01:03 +0100

    some testings

--- scripts/post_stage.php
+++ scripts/post_stage.php
@@ -27,7 +27,7 @@
  *   in hook scripts
  */
 $composer = ZS_APPLICATION_BASE_DIR.'/composer.phar';
-$success = exec("ls ".$composer." > /tmp/deploy.log", $retval);
+$success = exec("ls  > /tmp/deploy.log", $retval);
 #file_put_contents("/tmp/deploy.log", "/usr/local/zend/bin/php ".$composer." install");
 if ($success === false) {
     exit(1);
